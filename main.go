2e4ed7deae99450361dce60b80405eba0ea73172
Cruz000Testings <cruz000testing@outlook.com>
2025-10-14 12:24:43 -0600

    Initial commit

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/cruz000testings/go-gorm-resapi/db"
+	"github.com/cruz000testings/go-gorm-resapi/models"
+	"github.com/cruz000testings/go-gorm-resapi/routes"
+	"github.com/gorilla/mux"
+)
+
+func main() {
+
+	db.DBConnection()
+
+	db.DB.AutoMigrate(models.Task{})
+	db.DB.AutoMigrate(models.User{})
+
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", routes.HomeHandler)
+
+	r.HandleFunc("/users", routes.GetUsersHandler).Methods("GET")
+	r.HandleFunc("/users/{id}", routes.GetUserHandler).Methods("GET")
+	r.HandleFunc("/users", routes.PostUserHandler).Methods("POST")
+	r.HandleFunc("/users/{id}", routes.PatchUserHandler).Methods("PATCH")
+	r.HandleFunc("/users/{id}", routes.DeleteUserHandler).Methods("DELETE")
+
+	//Tasks routes
+	r.HandleFunc("/tasks", routes.GetTasksHandler).Methods("GET")
+	r.HandleFunc("/tasks/{id}", routes.GetTaskHandler).Methods("GET")
+	r.HandleFunc("/tasks", routes.CreateTaskHandler).Methods("POST")
+	r.HandleFunc("/tasks/{id}", routes.PatchUserHandler).Methods("PATCH")
+	r.HandleFunc("/tasks/{id}", routes.DeleteTaskHandler).Methods("DELETE")
+
+	http.ListenAndServe(":3000", r)
+}
---
+++ routes/tasks.routes.go
@@ -0,0 +1,104 @@
+package routes
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/cruz000testings/go-gorm-resapi/db"
+	"github.com/cruz000testings/go-gorm-resapi/models"
+	"github.com/gorilla/mux"
+)
+
+func GetTasksHandler(w http.ResponseWriter, r *http.Request) {
+	var tasks []models.Task
+	db.DB.Find(&tasks)
+	json.NewEncoder(w).Encode(tasks)
+}
+
+func GetTaskHandler(w http.ResponseWriter, r *http.Request) {
+	var task models.Task
+	params := mux.Vars(r)
+	db.DB.First(&task, params["id"])
+
+	if task.ID == 0 {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("Task not found :O"))
+		return
+	}
+
+	json.NewEncoder(w).Encode(&task)
+}
+
+func CreateTaskHandler(w http.ResponseWriter, r *http.Request) {
+	var task models.Task
+	jsonErr := json.NewDecoder(r.Body).Decode(&task)
+
+	if jsonErr != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("Invalid JSON"))
+		return
+	}
+
+	createdTask := db.DB.Create(&task)
+	createErr := createdTask.Error
+
+	if createErr != nil {
+		w.WriteHeader(http.StatusBadRequest) // 400
+		w.Write([]byte(createErr.Error()))
+		return
+	}
+
+	json.NewEncoder(w).Encode(&task)
+	w.WriteHeader(http.StatusCreated)
+}
+
+func PatchTaskHandler(w http.ResponseWriter, r *http.Request) {
+	var task models.Task
+	params := mux.Vars(r)
+	db.DB.First(&task, params["id"])
+
+	if task.ID == 0 {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("User not found :O"))
+		return
+	}
+
+	// Mapping to filter PATCH fields requested
+	var updates map[string]any
+	jsonErr := json.NewDecoder(r.Body).Decode(&updates)
+	if jsonErr != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("Invalid JSON"))
+		return
+	}
+
+	// Delete fields that should not be updated
+	delete(updates, "id")
+	delete(updates, "created_at")
+	delete(updates, "updated_at")
+
+	updatedTask := db.DB.Model(&task).Updates(updates).First(&task)
+	updateErr := updatedTask.Error
+	if updateErr != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(updateErr.Error()))
+		return
+	}
+
+	json.NewEncoder(w).Encode(&task)
+}
+
+func DeleteTaskHandler(w http.ResponseWriter, r *http.Request) {
+	var task models.Task
+	params := mux.Vars(r)
+	db.DB.First(&task, params["id"])
+
+	if task.ID == 0 {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("Task not found :O"))
+		return
+	}
+
+	db.DB.Unscoped().Delete(&task)
+	w.WriteHeader(http.StatusNoContent) // 204
+}
